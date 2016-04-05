0603e7077406ef1bc9a39f8b4b7ebf5c0d95d0f9
sergioC2 <sergio.cadavid@correounivalle.edu.co>
2016-04-04 21:24:11 -0500

    taller1 JVera-SCadavid
    Taller C++

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Taller1
+// Authoree    : Jairo Vera, Sergio Cadavid
+// Version     : 1
+// Copyright   :
+// Description : Crea un Árbitro e indica el inicio del juego
+//============================================================================
+
+#include <iostream>
+#include "Arbitro.h"
+using namespace std;
+
+int main()
+{
+    Arbitro arbitro = Arbitro();
+    arbitro.iniciarJuego();
+
+	return 0;
+}
